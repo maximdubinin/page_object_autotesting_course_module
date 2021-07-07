18efbe8d1678870f190602c013f8c6cbcff65cb9
maximdubinin <dubininmaxim@gmail.com>
2021-07-07 16:24:04 +0300

    should_be_login_link assertion added

--- conftest.py
+++ conftest.py
@@ -13,6 +13,7 @@ def browser(request):
     user_language = request.config.getoption("language")
     print("\nstart chrome browser for test..")
     options = Options()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
