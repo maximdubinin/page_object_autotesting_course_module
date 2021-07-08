e992a0d14819404bbacf6a236febdb8cf5128949
maximdubinin <dubininmaxim@gmail.com>
2021-07-08 17:48:15 +0300

    test methods updating and some refactored

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,6 +10,14 @@ class ProductPage(BasePage):
         button.click()
         self.solve_quiz_and_get_code()
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but should be"
+
     def should_be_added_to_basket_with_product_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_TO_BASKET_PRODUCT_NAME)
